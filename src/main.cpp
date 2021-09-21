a2add3256933dd70ed65791a1ec8acea4394a84f
ocineh <ocineh@hotmail.com>
2021-09-21 14:50:42 +0200

    Add directional arrows for turning

--- src/main.cpp
+++ src/main.cpp
@@ -23,17 +23,17 @@ int main(){
 		while(SDL_PollEvent(&events)){
 			switch(events.type){
 				case SDL_QUIT: isOpen = false; break;
-				case SDL_KEYDOWN:
-					SDL_Log("+key");
-					switch(events.key.keysym.scancode){
-						case SDL_SCANCODE_W: snake.turn(Up); break;
-						case SDL_SCANCODE_A: snake.turn(Left); break;
-						case SDL_SCANCODE_S: snake.turn(Down); break;
-						case SDL_SCANCODE_D: snake.turn(Right); break;
-						default: break;
-					}
-					break;
+				case SDL_KEYDOWN: SDL_Log("+key");
+					if(events.key.keysym.scancode == SDL_SCANCODE_W || events.key.keysym.scancode == SDL_SCANCODE_UP)
+						snake.turn(Up);
+					else if(events.key.keysym.scancode == SDL_SCANCODE_S || events.key.keysym.scancode == SDL_SCANCODE_DOWN)
+						snake.turn(Down);
+					else if(events.key.keysym.scancode == SDL_SCANCODE_A || events.key.keysym.scancode == SDL_SCANCODE_LEFT)
+						snake.turn(Left);
+					else if(events.key.keysym.scancode == SDL_SCANCODE_D || events.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+						snake.turn(Right); break;
 				case SDL_KEYUP: SDL_Log("-key"); break;
+				default: break;
 			}
 		}
 		
@@ -43,9 +43,9 @@ int main(){
 		snake.render(pRenderer);
 		
 		SDL_RenderPresent(pRenderer);
-		SDL_Delay(500);
+		SDL_Delay(50);
 	}
-
+	
 	SDL_DestroyRenderer(pRenderer);
 	SDL_DestroyWindow(pWindow);
 	SDL_Quit();
